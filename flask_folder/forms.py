7c052b60b60d27a2a7ec3525525b03a597296038
siswarnava-sss <sinhaswarnava@gmail.com>
2022-04-09 12:26:09 +0530

    updated

--- flask_folder/forms.py
+++
@@ -1,7 +0,0 @@
-from argon2 import PasswordHasher
-from flask_wtf import FlaskForm
-from wtforms imoprt StringField, SubmitField
-
-Class GettngCompanies():
-    names=StringField('names')
-    submit=SubmitField('submit')
